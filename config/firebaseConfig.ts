60ad5eb2b10417445aa6c39c2c97b0014874a81c
Chai Wei Jian <chaiweijian@protonmail.com>
2021-08-26 09:26:22 +0800

    chore: - setup firebase - remove favicon and Vercel icon

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCa1eYyHyqHHoedBTOiyvtD9KrdOlQFxPk",
+  authDomain: "group-wallet-322906.firebaseapp.com",
+  projectId: "group-wallet-322906",
+  storageBucket: "group-wallet-322906.appspot.com",
+  messagingSenderId: "226773022283",
+  appId: "1:226773022283:web:4577159afac2b091987fce",
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,25 @@
-import type {AppProps} from 'next/app'
-import {ChakraProvider} from "@chakra-ui/react"
-import Head from 'next/head'
-import {initializeApp, getApps} from 'firebase/app';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCa1eYyHyqHHoedBTOiyvtD9KrdOlQFxPk",
-    authDomain: "group-wallet-322906.firebaseapp.com",
-    projectId: "group-wallet-322906",
-    storageBucket: "group-wallet-322906.appspot.com",
-    messagingSenderId: "226773022283",
-    appId: "1:226773022283:web:4577159afac2b091987fce",
-};
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import { getApps, initializeApp } from "firebase/app";
+import { firebaseConfig } from "../config/firebaseConfig";
 
 if (getApps().length === 0) {
-    initializeApp(firebaseConfig);
+  initializeApp(firebaseConfig);
 }
 
-function MyApp({Component, pageProps}: AppProps) {
-    return <div>
-        <Head>
-            <title>Group Wallet</title>
-            <meta name="description" content="Group Wallet"/>
-        </Head>
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Head>
+        <title>Group Wallet</title>
+        <meta name="description" content="Group Wallet" />
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
